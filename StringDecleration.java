4e817b21aa62796ba290dd04d026b8625ceee050
Pranjal-ux <pranjal2shukla@gmail.com>
2025-09-15 10:26:37 +0530

    #Lecture1

---
+++ StringDecleration.java
@@ -0,0 +1,31 @@
+public class StringDecleration {
+    public static void main(String[] args) {
+
+// String name1 = "Pranjal";
+// String name2 = name1;
+// name2 = "Harsh";
+// System.out.println(name1);
+// System.out.println(name2);        
+
+// String name1 =" Saurabh"; 
+// String name2 = "Harry";
+// String name3 = "Harry";
+// String name4 = "Harry";
+// String name5 = "Mr.funsi";
+// System.out.println(name1==name2);
+// System.out.println(name3==name2);
+// System.out.println(name4==name5);
+// System.out.println(name1==name5);
+// System.out.println(name2==name4);
+
+String name1= new String("Pranjal");
+String name2= new String("harsh");
+String name3= new String("rohan");
+String name4= new String("Laulita");
+System.out.println(name1 == name2);
+System.out.println(name2 == name3);
+System.out.println(name3 == name4);
+System.out.println(name1 == name4);
+
+    }
+}
---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,1 @@
+"Chalna Sikhle";
